55517c2242d5bd6c00f33a9cecde947b9acd63dd
Sebastian Wallin <sebastian.wallin@gmail.com>
2013-07-24 07:55:28 +0200

    fix(tests): build spec file in correct directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
 		    files: [
 		      {
 		        expand: true,
-		        cwd: 'src',
+		        cwd: 'spec',
 		        src: "tests.coffee",
 		        dest: "build/spec",
 		        ext: ".js"
